a2a838db7d74030303a89f73c9870fd7eba300df
Nikita Marshalkin <marnikitta@gmail.com>
2025-01-03 20:49:34 +0100

    Refactor FastAPI to be a class rather than global functions in global in module

--- livecoding/main.py
+++ livecoding/main.py
@@ -5,18 +5,17 @@ import sys
 import time
 from contextlib import asynccontextmanager
 from pathlib import Path
-from typing import Annotated, Optional
+from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, WebSocket, HTTPException, Depends
+from fastapi import FastAPI, WebSocket, HTTPException, APIRouter
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse, PlainTextResponse
-
 from starlette.websockets import WebSocketDisconnect
 
-from livecoding.settings import settings
 from livecoding.model import WsMessage, SetSiteId, RoomModel
 from livecoding.room import Room, Site, RoomRepository, FullLogException
+from livecoding.settings import settings
 from livecoding.utils import try_notify_systemd, format_uptime, get_stars
 
 
@@ -31,111 +30,134 @@ def configure_logging():
 
 configure_logging()
 logger = logging.getLogger(__name__)
-started_at = datetime.datetime.now()
-
-room_repository: RoomRepository = RoomRepository(root=Path("./data"))
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    fl = asyncio.create_task(flush_task())
-    pr = asyncio.create_task(rooms_purge_task())
-    try_notify_systemd()
-    yield
-    logger.info("Terminating application. Flushing all rooms")
-    room_repository.flush_rooms()
-    fl.cancel()
-    pr.cancel()
+class LivecodingApp:
+    def __init__(self, serve_static: bool = True):
+        self.started_at = datetime.datetime.now()
 
+        self.app: FastAPI = FastAPI(lifespan=self.lifespan)
+        self.room_repository: RoomRepository = RoomRepository(root=Path("./data"))
+        self.app.mount("/", self.init_router(serve_static))
 
-app = FastAPI(lifespan=lifespan)
+    def init_router(self, serve_static: bool) -> APIRouter:
+        result = APIRouter()
 
+        resource_router = APIRouter()
+        resource_router.add_api_route("/health", self.health)
+        resource_router.add_api_route("/room", self.create_room, methods=["POST"])
+        resource_router.add_api_route("/room/{room_id}", self.get_room)
+        resource_router.add_api_websocket_route("/room/{room_id}/ws", self.websocket_endpoint)
+        resource_router.add_api_route("/intro.js", self.get_intro, response_class=PlainTextResponse)
 
-@app.get("/resources/health")
-async def health():
-    return {"status": "ok"}
+        result.mount("/resource", resource_router)
 
+        if serve_static:
+            static_router = APIRouter()
+            static_router.add_api_route("/", self.index)
+            static_router.add_api_route("/room/{room_id}", self.room_index)
+            static_router.mount("/public", app=StaticFiles(directory="./frontend/public"))
+            result.mount("/", static_router)
 
-@app.post("/resource/room")
-async def create_room() -> RoomModel:
-    room: Room = room_repository.create()
-    return RoomModel(roomId=room.room_id, events=room.get_model_events())
+        return result
 
+    @asynccontextmanager
+    async def lifespan(self, _: FastAPI):
+        fl = asyncio.create_task(self.flush_task())
+        pr = asyncio.create_task(self.rooms_purge_task())
+        try_notify_systemd()
+        yield
+        logger.info("Terminating application. Flushing all rooms")
+        self.room_repository.flush_rooms()
+        fl.cancel()
+        pr.cancel()
 
-def room_provider(room_id: str) -> Room:
-    if not room_repository.exists(room_id):
-        raise HTTPException(status_code=404, detail=f"Room {room_id} not found")
+    @staticmethod
+    async def health() -> dict[str, str]:
+        return {"status": "ok"}
 
-    return room_repository.get(room_id)
+    async def create_room(self) -> RoomModel:
+        room = self.room_repository.create()
+        return RoomModel(roomId=room.room_id, events=room.get_model_events())
 
+    def get_room_or_throw(self, room_id: str) -> Room:
+        if not self.room_repository.exists(room_id):
+            raise HTTPException(status_code=404, detail=f"Room {room_id} not found")
 
-@app.get("/resource/room/{room_id}")
-async def get_room(room: Annotated[Room, Depends(room_provider)]) -> RoomModel:
-    return RoomModel(roomId=room.room_id, events=room.get_model_events())
+        return self.room_repository.get(room_id)
 
+    async def get_room(self, room_id: str) -> RoomModel:
+        room = self.get_room_or_throw(room_id)
+        return RoomModel(roomId=room.room_id, events=room.get_model_events())
 
-@app.websocket("/resource/room/{room_id}/ws")
-async def websocket_endpoint(room: Annotated[Room, Depends(room_provider)], websocket: WebSocket, offset: int = 0):
-    await websocket.accept()
-    room_repository.claim(room.room_id)
+    async def websocket_endpoint(self, websocket: WebSocket, room_id: str, offset: int = 0):
+        room = self.get_room_or_throw(room_id)
 
-    site_id = room.get_next_site_id()
-    site = Site(site_id, websocket)
+        await websocket.accept()
+        self.room_repository.claim(room.room_id)
 
-    heartbit_task: Optional[asyncio.Task] = None
+        site_id = room.get_next_site_id()
+        site = Site(site_id, websocket)
 
-    try:
-        await room.connect(site, offset)
-        assert await websocket.receive_text() == "Hello", "First message must be Hello"
+        heartbit_task: Optional[asyncio.Task] = None
 
-        await site.send_message(WsMessage(setSiteId=SetSiteId(siteId=site_id)))
-        heartbit_task = asyncio.create_task(site.heartbit_task(settings.heartbit_interval))
+        try:
+            await room.connect(site, offset)
+            assert await websocket.receive_text() == "Hello", "First message must be Hello"
 
+            await site.send_message(WsMessage(setSiteId=SetSiteId(siteId=site_id)))
+            heartbit_task = asyncio.create_task(site.heartbit_task(settings.heartbit_interval))
+
+            while True:
+                msg = await site.receive_message()
+
+                if msg.crdtEvents is not None:
+                    await room.apply_events(msg.crdtEvents, sender=site_id)
+                    if room.events_len > settings.events_compaction_limit:
+                        await self.room_repository.compact_room(room.room_id)
+                elif msg.sitePresence is not None:
+                    await room.apply_presence(msg.sitePresence, sender=site_id)
+                else:
+                    raise ValueError(f"Invalid message: {msg}")
+        except FullLogException:
+            logger.error(f"Room {room.room_id} is full")
+        except WebSocketDisconnect:
+            pass
+        finally:
+            await room.disconnect(site_id)
+            if heartbit_task is not None:
+                heartbit_task.cancel()
+
+    async def flush_task(self):
         while True:
-            msg = await site.receive_message()
+            await asyncio.sleep(settings.rooms_flush_interval)
+            self.room_repository.flush_rooms()
+            await self.room_repository.gc()
 
-            if msg.crdtEvents is not None:
-                await room.apply_events(msg.crdtEvents, sender=site_id)
-                if room.events_len > settings.events_compaction_limit:
-                    await room_repository.compact_room(room.room_id)
-            elif msg.sitePresence is not None:
-                await room.apply_presence(msg.sitePresence, sender=site_id)
-            else:
-                raise ValueError(f"Invalid message: {msg}")
-    except FullLogException:
-        logger.error(f"Room {room.room_id} is full")
-    except WebSocketDisconnect:
-        pass
-    finally:
-        await room.disconnect(site_id)
-        if heartbit_task is not None:
-            heartbit_task.cancel()
+    async def rooms_purge_task(self):
+        while True:
+            self.room_repository.purge_stale_rooms(settings.rooms_ttl_days)
+            # on start-up and then hourly
+            await asyncio.sleep(60 * 60)
 
+    @staticmethod
+    async def index():
+        return FileResponse("frontend/index.html")
 
-async def flush_task():
-    while True:
-        await asyncio.sleep(settings.rooms_flush_interval)
-        room_repository.flush_rooms()
-        await room_repository.gc()
+    # noinspection PyUnusedLocal
+    @staticmethod
+    async def room_index(room_id: str):
+        return FileResponse("frontend/index.html")
 
+    async def get_intro(self) -> str:
+        active_rooms = len(self.room_repository.rooms)
+        active_users = sum(len(room.sites) for room in self.room_repository.rooms.values())
+        # smart way to cache for 5 seconds
+        total_rooms = self.room_repository.total_rooms(round(time.time() / 30))
+        uptime = format_uptime(self.started_at, datetime.datetime.now())
+        stars = get_stars(settings.repository, round(time.time() / 60))
 
-async def rooms_purge_task():
-    while True:
-        room_repository.purge_stale_rooms(settings.rooms_ttl_days)
-        # on start-up and then hourly
-        await asyncio.sleep(60 * 60)
-
-
-@app.get("/resource/intro.js", response_class=PlainTextResponse)
-async def get_intro() -> str:
-    active_rooms = len(room_repository.rooms)
-    active_users = sum(len(room.sites) for room in room_repository.rooms.values())
-    # smart way to cache for 5 seconds
-    total_rooms = room_repository.total_rooms(round(time.time() / 30))
-    uptime = format_uptime(started_at, datetime.datetime.now())
-    stars = get_stars(settings.repository, round(time.time() / 60))
-
-    return f"""// Welcome!
+        return f"""// Welcome!
 //
 // 1. Create a new room
 // 2. Share the link with friends
@@ -167,22 +189,11 @@ const serverConfig = {{
 """
 
 
-@app.get("/")
-async def index():
-    return FileResponse("frontend/index.html")
-
-
-@app.get("/room/{room_id}")
-async def room_index(room_id: str):
-    print(room_id)
-    return FileResponse("frontend/index.html")
-
-
-app.mount("/public", app=StaticFiles(directory="./frontend/public"))
-
 if __name__ == "__main__":
+    app = LivecodingApp()
+
     uvicorn.run(
-        app,
+        app.app,
         host="localhost",
         access_log=True,
         log_config=None,
